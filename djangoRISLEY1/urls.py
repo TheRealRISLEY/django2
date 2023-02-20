647eca06abd0686574e78b458ba21cecd7b2b671
luthor628 <abdulrahmanabdulaziz628@gmail.com>
2023-02-20 13:17:14 +0300

    Django

--- djangoRISLEY1/urls.py
+++ djangoRISLEY1/urls.py
@@ -17,12 +17,12 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
-
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('about/', views.about, name = 'about'),
-    path('home/', views.home, name = 'home'),
-    path('contacts/', views.contacts, name = 'contacts')
+    path('about/', views.about, name='about'),
+    path('home/', views.home, name='home'),
+    path('contacts/', views.contacts, name='contacts'),
+    path('index/', views.index, name='index'),
+    path('mypage/', views.newpage, name='mypage')
 
 ]
--- djangoRISLEY1/views.py
+++ djangoRISLEY1/views.py
@@ -4,8 +4,17 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'home.html')
 
+
 def about(request):
     return render(request, 'about.html')
 
+
 def contacts(request):
     return render(request, 'contacts.html')
+
+
+def index(request):
+    return render(request, 'index.html')
+
+def newpage(request):
+    return render(request, 'page_my.html')
